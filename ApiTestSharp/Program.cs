c24f7c795467aa577c525078f51436712ba172cb
Skippn <landerdekesel@gmail.com>
2023-03-09 15:35:37 +0100

    Add project files.

---
+++ ApiTestSharp/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+using System.Security.Principal;
+using BlitzWare.Utilities;
+using System.Diagnostics;
+
+namespace BlitzWare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            API.OnProgramStart.Initialize("BlitzWare Test", "3f842f1ad6f7eeace6a87bbe51a00e15db6642b354a420eeba105c2fbd0eda2b", "1.0");
+
+            Misc.RandomTitle();
+            Misc.Logo1();
+            Console.WriteLine("\n[1] Login");
+            Console.WriteLine("[2] Register");
+            Console.WriteLine("[3] Extend Subscription");
+            Console.WriteLine("\nOption:");
+            string option = Console.ReadLine();
+
+            if (option == "1")
+            {
+                Console.Clear();
+                Misc.Logo1();
+                Console.Write("\nUsername: ");
+                string username = Console.ReadLine();
+                Console.Write("\nPassword: ");
+                string password = Console.ReadLine();
+
+                if (API.Login(username, password))
+                {
+                    MessageBox.Show("Successfully Logged In!", API.OnProgramStart.Name, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Console.Clear();
+                    Misc.Logo1();
+                    Console.WriteLine("ID: " + API.User.ID);
+                    Console.WriteLine("Username: " + API.User.Username);
+                    Console.WriteLine("Email: " + API.User.Email);
+                    Console.WriteLine("Subscription Expiry: " + API.User.Expiry);
+                    Console.WriteLine("HWID: " + API.User.HWID);
+                    Console.WriteLine("Last Login: " + API.User.LastLogin);
+                    Console.WriteLine("IP: " + API.User.IP);
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Process.GetCurrentProcess().Kill();
+                }
+            }
+            else if (option == "2")
+            {
+                Console.Clear();
+                Misc.Logo1();
+                Console.Write("\nUsername: ");
+                string username = Console.ReadLine();
+                Console.Write("\nPassword: ");
+                string password = Console.ReadLine();
+                Console.Write("\nEmail: ");
+                string email = Console.ReadLine();
+                Console.Write("\nLicense: ");
+                string license = Console.ReadLine();
+
+                if (API.Register(username, password, email, license))
+                {
+                    MessageBox.Show("Successfully Registered!", API.OnProgramStart.Name, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.ReadLine();
+                    Process.GetCurrentProcess().Kill();
+                }
+            }
+            else if (option == "3")
+            {
+                Console.Clear();
+                Misc.Logo1();
+                Console.Write("\nUsername: ");
+                string username = Console.ReadLine();
+                Console.Write("\nPassword: ");
+                string password = Console.ReadLine();
+                Console.Write("\nLicense: ");
+                string license = Console.ReadLine();
+
+                if (API.ExtendSub(username, password, license))
+                {
+                    MessageBox.Show("Successfully Extended Your Subscription!", API.OnProgramStart.Name, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Process.GetCurrentProcess().Kill();
+                }
+            }
+        }
+    }
+}
---
+++ ApiTestSharp/Utilities/Misc.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlitzWare.Utilities
+{
+    public static class Misc
+    {
+        public static void Logo1()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+            Console.WriteLine("██████╗░██╗░░░░░██╗████████╗███████╗░██╗░░░░░░░██╗░█████╗░██████╗░███████╗");
+            Console.WriteLine("██╔══██╗██║░░░░░██║╚══██╔══╝╚════██║░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝");
+            Console.WriteLine("██████╦╝██║░░░░░██║░░░██║░░░░░███╔═╝░╚██╗████╗██╔╝███████║██████╔╝█████╗░░");
+            Console.WriteLine("██╔══██╗██║░░░░░██║░░░██║░░░██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗██╔══╝░░");
+            Console.WriteLine("██████╦╝███████╗██║░░░██║░░░███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║███████╗");
+            Console.WriteLine("╚═════╝░╚══════╝╚═╝░░░╚═╝░░░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚══════╝");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+        public static void Logo2()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+            Console.WriteLine("██████╗░██╗░░░░░██╗████████╗███████╗░██╗░░░░░░░██╗░█████╗░██████╗░███████╗");
+            Console.WriteLine("██╔══██╗██║░░░░░██║╚══██╔══╝╚════██║░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝");
+            Console.WriteLine("██████╦╝██║░░░░░██║░░░██║░░░░░███╔═╝░╚██╗████╗██╔╝███████║██████╔╝█████╗░░");
+            Console.WriteLine("██╔══██╗██║░░░░░██║░░░██║░░░██╔══╝░░░░████╔═████║░██╔══██║██╔══██╗██╔══╝░░");
+            Console.WriteLine("██████╦╝███████╗██║░░░██║░░░███████╗░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║███████╗");
+            Console.WriteLine("╚═════╝░╚══════╝╚═╝░░░╚═╝░░░╚══════╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚══════╝");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+        public static void RandomTitle()
+        {
+            {
+                int size = 15;
+                char[] chars = new char[62];
+                string a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+                chars = a.ToCharArray();
+
+                RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider();
+
+                byte[] data = new byte[size];
+                crypto.GetNonZeroBytes(data);
+
+                StringBuilder result = new StringBuilder(size);
+
+                foreach (byte b in data)
+                    result.Append(chars[b % (chars.Length - 1)]);
+                Console.Title = "" + result;
+
+            }
+        }
+    }
+}
